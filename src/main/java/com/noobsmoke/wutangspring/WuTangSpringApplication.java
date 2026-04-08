fdb945d848ac6e39ce4e69526a2c0b6137eeaa20
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-04-08 17:24:49 -0500

    Skeleton

---
+++ src/main/java/com/noobsmoke/wutangspring/WuTangSpringApplication.java
@@ -0,0 +1,13 @@
+package com.noobsmoke.wutangspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuTangSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuTangSpringApplication.class, args);
+    }
+
+}
